8fe9c82c7aa93f60325c4502bb74858e9c2bc170
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-02-13 15:08:39 -0300

    add new exercises for totaltypescript couse

--- src/03-optional-properties.problem.ts
+++ src/03-optional-properties.problem.ts
@@ -1,12 +1,18 @@
 import { expect, it } from "vitest";
 
-export const getName = (params: { first: string; last: string }) => {
+interface Params {
+  first: string;
+  last?: string;
+}
+
+export const getName = (params: Params) => {
   if (params.last) {
     return `${params.first} ${params.last}`;
   }
   return params.first;
 };
 
+// TEST
 it("Should work with just the first name", () => {
   const name = getName({
     first: "Matt",
